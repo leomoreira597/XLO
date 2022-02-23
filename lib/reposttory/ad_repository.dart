5c30f7e2a516e519acc044c56c13472366f3334c
Leonardo Moreira <lmoreira597@gmail.com>
2022-02-23 18:07:56 -0300

    Publicando os anuncios ainda com erros

--- lib/reposttory/ad_repository.dart
+++ lib/reposttory/ad_repository.dart
@@ -3,10 +3,37 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:xlo_mobx/models/ad.dart';
 import 'package:path/path.dart' as path;
 import 'package:xlo_mobx/reposttory/parse_errors.dart';
+import 'package:xlo_mobx/reposttory/table_keys.dart';
 
 class AdRepository {
   Future<void> save(AD ad) async{
     final parseImages = await saveImages(ad.images);
+
+    final parseUser = ParseUser('','','')..set(keyUserId, ad.user!.id);
+
+    final adObject = ParseObject(keyAdTable);
+
+    final parseAcl = ParseACL(owner: parseUser);
+    parseAcl.setPublicReadAccess(allowed: true);
+    parseAcl.setPublicWriteAccess(allowed: false);
+    adObject.setACL(parseAcl);
+
+    adObject.set<String?>(keyAdTitle, ad.title);
+    adObject.set<String?>(keyAdDescription, ad.description);
+    adObject.set<bool?>(keyAdHidePhone, ad.hidePhone);
+    adObject.set<num?>(keyAdPrice, ad.price);
+    adObject.set<int>(keyAdStatus, ad.status.index);
+    adObject.set<String?>(keyAdDistrict, ad.address!.district);
+    adObject.set<String?>(keyAdCity, ad.address!.city!.name);
+    adObject.set<String?>(keyAdFederativeUnit, ad.address!.uf!.initials);
+    adObject.set<String?>(keyAdPostalCode, ad.address!.cep);
+    adObject.set<List<ParseFile>>(keyAdImages, parseImages);
+    adObject.set<ParseUser>(keyAdOwner, parseUser);
+    adObject.set<ParseObject>(keyAdCategory,
+        ParseObject(keyCategoriesTable)..set(keyCategoryId, ad.category!.id));
+
+    final response = await adObject.save();
+    print(response.success);
   }
 
   Future<List<ParseFile>> saveImages(List images) async {
--- lib/reposttory/table_keys.dart
+++ lib/reposttory/table_keys.dart
@@ -5,6 +5,23 @@ const keyUserPhone = 'phone';
 const keyUserPassword = 'password';
 const keyUserType = 'type';
 const keyUserCreatedAt = 'createdAt';
-
 const keyCategoriesTable = 'Categories';
+const keyCategoryId = 'objectId';
 const keyCategoryDescription = 'description';
+
+const String keyAdTable = 'Ad';
+const String keyAdId = 'objectId';
+const String keyAdTitle = 'tile';
+const String keyAdDescription = 'description';
+const String keyAdHidePhone = 'hidePhone';
+const String keyAdPrice = 'price';
+const String keyAdStatus = 'status';
+const String keyAdDistrict = 'district';
+const String keyAdCity = 'city';
+const String keyAdPostalCode = 'postalCode';
+const String keyAdFederativeUnit = 'federativeUnit';
+const String keyAdImages = 'images';
+const String keyAdCategory = 'category';
+const String keyAdOwner = 'owner';
+const String keyAdCreatedAt = 'createdAt';
+const String keyAdViews = 'views';
